99631a00a2dc1dfa155c9fdac903340f66c6ce93
Isaac Curtiss <ijcurtiss7@gmail.com>
2019-05-16 14:48:37 -0700

    auto check the logged in status of a user when the application/components loads

--- reactjs-frontend/react-authFrontEnd/src/components/app.js
+++ reactjs-frontend/react-authFrontEnd/src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Dashboard from './Dashboard';
+import axios from 'axios';
 
 export default class App extends Component {
 
@@ -14,6 +15,34 @@ export default class App extends Component {
 
     this.handleLogin = this.handleLogin.bind(this);
 
+  }
+
+  checkLoginStatus() { 
+    //created the logged_in endpoint in the Rails API
+    axios.get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then(response => {
+        //if the logged_in is true and state says not logged in, change it to logged in and set user to THAT particular user
+        if(response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN",
+            user: response.data.user
+          })
+        } 
+        //if the logged_in is false and loggedInStatus says they're logged in, then turn it off/change it 
+        //if local state is saying a user is logged in but the system/API says they are not logged in then defer to the API
+        else if(!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            user: {}
+          })
+        } 
+      }).catch(error => {
+        console.log("check login error", error)
+      })
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
   }
 
   handleLogin(data) {
