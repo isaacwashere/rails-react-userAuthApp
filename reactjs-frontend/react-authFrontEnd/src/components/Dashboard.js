300ab283ee218f515595540a29723c2a0f63aeed
Isaac Curtiss <ijcurtiss7@gmail.com>
2019-05-16 11:29:38 -0700

    initilialized set up of reactjs frontend

---
+++ reactjs-frontend/react-authFrontEnd/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return( 
+    <div>
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    </div>
+  ); 
+}
+
+export default Dashboard;
+
+
